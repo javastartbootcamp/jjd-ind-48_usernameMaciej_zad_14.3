495e1691b4559bd0ad295dc0a0c9498fdbf8621e
usernameMaciej <maciej.przystarz@protonmail.com>
2023-02-02 14:07:26 +0100

    rozwiazanie zadania

---
+++ src/main/java/Country.java
@@ -0,0 +1,17 @@
+
+public class Country {
+    private String code;
+    private String name;
+    private long population;
+
+    public Country(String code, String name, long population) {
+        this.code = code;
+        this.name = name;
+        this.population = population;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + code + ") ma " + population + " ludności";
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class Main {
 
@@ -9,8 +11,47 @@ public class Main {
         main.run(scanner);
     }
 
-    void run(Scanner scanner) {
-        // usupełnij metodę
+    private void run(Scanner scanner) {
+        try {
+            Map<String, HashSet<Country>> countryMap = readCountriesFileToMap("countries.csv");
+            String country = getCountryFromUser(scanner);
+            printCountryFromUser(countryMap, country);
+        } catch (FileNotFoundException e) {
+            System.out.println("Brak pliku \"countries.csv\"");
+        }
+    }
+
+    private String getCountryFromUser(Scanner scanner) {
+        System.out.println("Podaj kod kraju, o którym chcesz zobaczyć informacje:");
+        return scanner.nextLine();
+    }
+
+    private Map<String, HashSet<Country>> readCountriesFileToMap(String fileName) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(fileName));
+        Map<String, HashSet<Country>> countryMap = new HashMap<>();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] split = line.split(";");
+            String code = split[0];
+            String countryName = split[1];
+            long population = Long.parseLong(split[2]);
+            Country country = new Country(code, countryName, population);
+            HashSet<Country> categorySet = new HashSet<>();
+            categorySet.add(country);
+            countryMap.put(code, categorySet);
+        }
+        return countryMap;
+    }
+
+    private void printCountryFromUser(Map<String, HashSet<Country>> countryMap, String countryCode) {
+        HashSet<Country> countries = countryMap.get(countryCode);
+        if (countries == null) {
+            System.out.println("Kod kraju " + countryCode + " nie został odnaleziony");
+        } else {
+            for (Country country : countries) {
+                System.out.println(country);
+            }
+        }
     }
 
 }
